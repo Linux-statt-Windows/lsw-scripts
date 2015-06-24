e6b1160340508b97f58ecc61c94a69b116a8e782
Milan Ihl <milanihl@gmail.com>
2015-06-24 16:17:20 +0200

    add config.lua

---
+++ telegram/telegram-bot/config/data/config.lua
@@ -0,0 +1,54 @@
+do local _ = {
+  disabled_channels = {},
+  enabled_plugins = {
+    "eur",
+    "btc",
+    "google",
+    "gps",
+    "help",
+    "images",
+    "img_google",
+    "location",
+    "plugins",
+    "stats",
+    "time",
+    "version",
+    "weather",
+    "meme",
+    "tweet",
+    "invite",
+    "hackernews",
+    "calculator",
+    "id",
+    "qr",
+    "yoda",
+    "imdb",
+    "isup",
+    "translate",
+    "webshot",
+    "wiki",
+    "tex",
+    "vote",
+    "twitter",
+    "trivia",
+    "steam",
+    "service_template",
+    "service_entergroup",
+    "roll",
+    "remind",
+    "search_youtube",
+    "lyrics",
+    "face",
+    "expand",
+    "dogify",
+    "quotes",
+    "lsw",
+    "youtube"
+  },
+  sudo_users = {
+    60392360
+  }
+}
+return _
+end
+
